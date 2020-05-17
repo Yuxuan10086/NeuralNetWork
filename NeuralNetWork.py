cc6e8ccd91660400c19a34adc79db0d09de61e34
张宇轩 <55274846+tkinter-zyx@users.noreply.github.com>
2020-05-17 12:52:51 +0800

    Update NeuralNetWork.py

--- NeuralNetWork.py
+++ NeuralNetWork.py
@@ -1,52 +1,59 @@
 import numpy as np
-class network:
-    def __init__(self, nodes, learn_rate): # nodes为列表,储存各层节点数
+
+# 在函数之间交互时一律使用二维矩阵
+
+class NeuralNetWork:
+    def __init__(self, nodes, learn_rate):
         self.nodes = nodes
         self.weight = []
         self.tier = len(nodes)
         # for i in range(self.tier):
         for i in range(len(nodes) - 1):
-            weight = np.random.normal(0.0, pow(nodes[i], -0.5), (nodes[i], nodes[i]))
+            weight = np.random.normal(0.0, pow(nodes[i], -0.5), (nodes[i + 1], nodes[i]))
             self.weight.append(weight)
         self.final_outputs = []
-        self.sigmoid = lambda x : 1 / (1 + pow(2.7182818, -x))
+        self.sigmoid = lambda x: 1 / (1 + pow(2.718, -x))
         self.learn_rate = learn_rate
         self.outputs_list = []
 
-    def query(self, inputs): # 传入输入列表
-        inputs = np.transpose(inputs)
+    def query(self, inputs):  # 传入输入列表
+        inputs = np.array(inputs, ndmin = 2).T
         self.outputs_list.append(inputs)
         for i in range(self.tier - 1):
-            # print(inputs)
-            # print(self.weight)
             inputs = np.dot(self.weight[i], inputs)
-            # print('inputs : ', inputs)
             for i in range(len(inputs)):
                 inputs[i] = self.sigmoid(inputs[i])
-            self.outputs_list.append(inputs)
-        self.final_outputs = inputs
-        return self.final_outputs
+            self.outputs_list.append(np.array(inputs, ndmin = 2))
+        self.final_outputs = self.outputs_list[-1]
+        # print(self.final_outputs)
 
-    def train(self, inputs, targets):  # 传入输入列表和期望输出列表
-        inputs = np.array(inputs).T
-        targets = np.array(targets).T
-        outputs = self.query(inputs).T
-        errors = [targets - outputs]  # 倒序储存所有层的误差,即第一个元素为输出层的误差
+    def train(self, inputs, targets): #传入列表
+        self.query(inputs)
+        # inputs = np.array(inputs, ndmin=2).T
+        targets = np.array(targets, ndmin=2).T
+        errors = [self.final_outputs - targets]
         for i in range(self.tier - 1):
-            self.weight[-i - 1] += self.learn_rate * np.dot((errors * outputs * (1 - outputs)),
-                                                            np.transpose(self.outputs_list[-i - 2]))
-            outputs = self.outputs_list[-i - 2]
-            errors = np.dot(self.weight[-i - 1].T, errors[i])
-        # print("ereors:", errors)
-
+            errors = [np.dot(self.weight[-i - 1].T, errors[-i - 1])] + errors
+        # print('errors', errors)
+        for j in range(self.tier - 1):
+            middle_entry = []
+            for i in range(self.nodes[j + 1]):
+                middle_entry.append(errors[j + 1][i] * self.outputs_list[j + 1][i] * (1 - self.outputs_list[j + 1][i]))
+            middle_entry = np.array(middle_entry, ndmin = 2)
+            # print('mid', middle_entry)
+            derta_weight = self.learn_rate * middle_entry * self.outputs_list[j].T
+            # print(derta_weight)
+            self.weight[j] -= derta_weight
+        # print('wei', self.weight)
 
 
     def check(self):
-        print(self.nodes, 'weight', self.weight, self.tier, "out", self.outputs_list)
+        print(self.nodes, 'weight', self.weight, self.tier, self.weight[0].shape)  #, "out", self.outputs_list)
 
-test = network([3, 5, 5, 5], 0.1)
-test.check()
-while 1:
-    test.train([0.5, 0.75, 0.7], [1, 0, 0])
-    print("res", test.query([0.5, 0.75, 0.7]))
-test.check()
+# test = NeuralNetWork([5, 300, 10], 0.05)
+# test.check()
+# while 1:
+#     test.query([0.7, 0.75, 0.8, 0.3, 0.5])
+#     test.train([0.7, 0.75, 0.8, 0.3, 0.5], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+#     print(test.final_outputs)
+# print('out', test.outputs_list, test.final_outputs.shape)
